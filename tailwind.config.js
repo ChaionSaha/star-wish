65dda663fe897348878a3a6af70c87e5baff3524
Chaion Saha <chaionsaha00@gmail.com>
2023-08-06 13:23:13 +0600

    fixed slider issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
 	content: ['./**/*.{html,js}', './node_modules/flowbite/**/*.js'],
 	theme: {
 		extend: {},
+		screens: {
+			sm: '576px',
+			md: '768px',
+			lg: '992px',
+			xl: '1200px',
+			'2xl': '1400px',
+		},
 	},
 
 	daisyui: {
